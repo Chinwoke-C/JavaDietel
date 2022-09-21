f4c383f8e4010c60b4fa6e2b3fbba4b10cc0208f
chinwoke <canugwara99@gmail.com>
2022-09-21 12:17:24 +0100

    firstly

---
+++ src/myPractices/Array.java
@@ -0,0 +1,16 @@
+package myPractices;
+
+public class Array {
+    public static void main(String[] args) {
+        //String [] colour = new String[7];
+     String [] colour = {"Red","Orange", "yellow","Green","Blue","Indigo", "Violet"};
+        for (int i = 0; i < colour.length ; i++) {
+            int a = i + 1;
+            System.out.printf("%n%d %s",a,colour[i]);
+
+
+        }
+        }
+
+    }
+
